c1a856e5a5676d8118ed1b857293f94af76bcb79
起风之城 <86582822+shijianzugouniai@users.noreply.github.com>
2022-07-12 10:17:56 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? "/vue-hextris/" : ""
+    publicPath: process.env.NODE_ENV === 'production' ? "/vue-hextris/" : "",
+    devServer: { disableHostCheck: true }
 };
